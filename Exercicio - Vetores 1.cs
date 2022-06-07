2591e3a22fcdef152d3f4b382dadeb9d2201e2eb
Tafarel Mello <tafarel.luis.mello@gmail.com>
2022-06-07 19:39:44 -0300

    Update Exercicio - Vetores 1.cs

--- Exercicio - Vetores 1.cs
+++ Exercicio - Vetores 1.cs
@@ -1,78 +1,62 @@
 using System;
-    class Program
+
+class Program 
+{
+  static void Main(string[] args) 
+  {
+
+    int[] vetorPrinc = new int [10];
+    int maiorValor = 0; 
+    int menorValor = 0; 
+    int valor;
+    int contaPares = 0; 
+    int contaImpares = 0;
+    int soma = 0;
+    Double Media = 0;
+    int contador = 1;
+    
+
+    while(contador <= 10)
     {
+      Console.Write($"Digite o {contador}o numero: ");
+      valor = int.Parse(Console.ReadLine());
 
-                // Vetor de inteiros com 10 posições.
-                //     a. Mostrar o maior e o menor valor do vetor
-                //     b. Mostrar quantos números pares e quantos números ímpares existem no vetor
-                //     c. Mostrar a média aritmética dos valores do vetor
+      if(contador == 1)
+      {
+        maiorValor = valor;
+        menorValor = valor;
+      }
+        if( valor > maiorValor )
+          maiorValor = valor;
+          else if( valor < menorValor )
+            menorValor = valor;
+
+      if(valor % 2 == 0)
+        contaPares++;
+      else
+        contaImpares++;
+
+      soma = soma + valor;
 
 
-        static void Main(string[] args)
-        {
-            int[] Vetor = new int[10];
-            for (int i = 0; i < 10; i++)
-            {
-                Console.Write($"Elemento {i + 1} do Vetor: ");
-                Vetor[i] = int.Parse(Console.ReadLine());
-            }
-            Console.WriteLine("\n");
-            
-            Console.WriteLine($"O Maior Valor do Vetor é {AchaMaior(Vetor)}");
-            Console.WriteLine($"O Menor Valor do Vetor é {AchaMenor(Vetor)}");
-            Console.WriteLine($"Existem {ContaPares(Vetor)} elementos Pares no Vetor");
-            Console.WriteLine($"Existem {ContaÍmpares(Vetor)} elementos Ímpares no Vetor");
-            Console.WriteLine($"A Média Aritmética dos elementos do vetor é {AchaMédia(Vetor):F2}");
-            Console.ReadKey();
-        }
-        static int AchaMaior(int[] V)
-        {
-            int Maior=V[0];
-            for (int i = 1; i < V.Length; i++)
-            {
-                if (V[i] > Maior)
-                    Maior = V[i];
-            }
-            return Maior;
-        }
-        static int AchaMenor(int[] V)
-        {
-            int Menor = V[0];
-            for (int i = 1; i < V.Length; i++)
-            {
-                if (V[i] < Menor)
-                    Menor = V[i];
-            }
-            return Menor;
-        }
-        static int ContaPares(int[] V)
-        {
-            int Pares = 0;
-            for (int i = 0; i < V.Length; i++)
-            {
-                if (V[i] % 2 == 0)
-                    Pares++;
-            }
-            return Pares;
-        }
-        static int ContaÍmpares(int[] V)
-        {
-            int Ímpares = 0;
-            for (int i = 0; i < V.Length; i++)
-            {
-                if (V[i] % 2 != 0)
-                    Ímpares++;
-            }
-            return Ímpares;
-        }
-        static double AchaMédia(int[] V)
-        {
-            int Soma=0;
-            double Média;
-            for (int i = 0; i < V.Length; i++)
-            {
-                Soma += V[i];
-            }
-            return (double)Soma / V.Length;
-        }
+      contador++;
     }
+
+    Media = soma / 10.0;
+
+    
+    Console.WriteLine($"\n");
+    Console.WriteLine($"O maior numero digitado foi: {maiorValor}.");
+    Console.WriteLine($"\n");
+    Console.WriteLine($"O menor numero digitado foi: {menorValor}.");
+    Console.WriteLine($"\n");
+    Console.WriteLine($"Foram digitados {contaPares} numeros pares.");
+    Console.WriteLine($"\n");
+    Console.WriteLine($"Foram digitados {contaImpares} numeros impares.");
+    Console.WriteLine($"\n");
+    Console.WriteLine($"A soma dos numeros digitados foi: {soma}.");
+    Console.WriteLine($"\n");
+    Console.WriteLine($"A media dos numeros digitados foi: {Media}.");
+    
+  }
+}
