45d9255a29b8384bdcf4cd1dace9b5acf130d715
Tafarel Mello <tafarel.luis.mello@gmail.com>
2022-07-09 12:47:07 -0300

    Commit

---
+++ 059-Exercicio - Conversor de temperatura do-while.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Globalization;
+
+namespace Curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double C, F;
+            char repetir;
+
+            do
+            {
+                Console.WriteLine("Digite uma temperatura em Celsius: ");
+                C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                F = ((9.0 * C) / 5.0) + 32.0;
+
+                Console.WriteLine("Equivalente em Fahrenheit: " + F.ToString("F1", CultureInfo.InvariantCulture));
+                Console.WriteLine("Deseja repetir (s/n)? ");
+                repetir = char.Parse(Console.ReadLine());
+
+            } while (repetir == 's');
+
+
+        }
+    }
+}
