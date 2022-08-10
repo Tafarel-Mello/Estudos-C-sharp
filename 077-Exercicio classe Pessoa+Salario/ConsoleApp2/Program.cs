4647601c3696f45c25424f58b8b68b4d8c5fb8ab
Tafarel Mello <tafarel.luis.mello@gmail.com>
2022-08-10 13:45:13 -0300

    Commit

---
+++ 077-Exercicio classe Pessoa+Salario/ConsoleApp2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+
+namespace Curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa pessoa1, pessoa2;
+            double soma, media;
+
+            pessoa1 = new Pessoa();
+            pessoa2 = new Pessoa();
+
+
+            Console.WriteLine("Digite seu nome: ");
+            pessoa1.A = Console.ReadLine();
+            Console.WriteLine("Agora digite seu salario: ");
+            pessoa1.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+
+            Console.WriteLine("Digite seu nome: ");
+            pessoa2.A = Console.ReadLine();
+            Console.WriteLine("Agora digite seu salario: ");
+            pessoa2.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            soma = pessoa1.B + pessoa2.B;
+            media = soma / 2.0;
+
+            Console.WriteLine("Salario medio: " + media.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
