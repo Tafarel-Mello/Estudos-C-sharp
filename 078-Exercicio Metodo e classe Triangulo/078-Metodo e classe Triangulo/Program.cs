7f3dad8d940d9597ae7a19a7710de4d30d4087ef
Tafarel Mello <tafarel.luis.mello@gmail.com>
2022-08-12 12:37:35 -0300

    Commit

---
+++ 078-Exercicio Metodo e classe Triangulo/078-Metodo e classe Triangulo/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Globalization;
+
+namespace Curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangulo x, y;
+            
+            x = new Triangulo();
+            y = new Triangulo();
+
+            Console.WriteLine("Entre as medidas do triangulo 'X': ");
+            x.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Agora entre as medidas do triangulo 'Y': ");
+            y.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            y.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            y.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double areaX = x.Area();
+            double areaY = y.Area();
+
+            if(areaX > areaY)
+            {
+                Console.WriteLine("Maior area: X");
+            }
+            else
+            {
+                Console.WriteLine("Maior area: Y");
+            }
+        }
+    }
+}
