08983d8780f4455e9541350e300d79c72576df69
Tafarel Mello <tafarel.luis.mello@gmail.com>
2022-08-12 20:45:00 -0300

    Commit

---
+++ 080-Exercicio metodo e classe Retangulo/ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+
+namespace Curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Retangulo x = new Retangulo();
+
+            Console.WriteLine("Entre a largura e altura do retangulo: ");
+            x.Largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.Altura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double area = x.Area();
+            double perimetro = x.Perimetro();
+            double diagonal = x.Diagonal();
+
+            Console.WriteLine("AREA = " + area.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("PERIMETRO = " + perimetro.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("DIAGONAL = " + diagonal.ToString("F2", CultureInfo.InvariantCulture));
+
+        }
+    }
+}
