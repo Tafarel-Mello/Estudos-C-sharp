b085f4c9e8329577b7bd1bf21866e0b4896422e0
Tafarel Mello <tafarel.luis.mello@gmail.com>
2022-08-13 17:10:17 -0300

    Commit

---
+++ ConsoleApp1/ConsoleApp1/Calculadora.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Curso
+{
+    class Calculadora
+    {
+        public static double IoF = 0.06;
+
+        public static double Converte(double cotacao, double comprar)
+        {
+            double total = cotacao * comprar; // entrada dos parametros para calculo
+            return total + total * IoF;       // retorno do valor final para o metodo 'Converte'
+        }
+
+
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace Curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Qual e a cotacao do dolar? ");
+            double cotacao = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Quantos dolares voce deseja comprar? ");
+            double comprar = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double valorFinal = Calculadora.Converte(cotacao, comprar); // chama o metodo fornecendo os parametros
+
+            Console.WriteLine("Valor final a ser pago em reais: " + valorFinal.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
