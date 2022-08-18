70330056c14b82e2ad6eed3d63f5da10338d73bf
Tafarel Mello <tafarel.luis.mello@gmail.com>
2022-08-18 20:07:34 -0300

    Commit

---
+++ 087-Exemplo Encapsulamento/087-Exemplo Encapsulamento/Produto.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Curso
+{
+    class Produto
+    {
+        private string _nome;
+        private double _preco;
+        private int _quantidade;
+
+
+
+
+        // construtores ***************
+        public Produto()
+        {
+
+        }
+
+        public Produto(string nome, double preco, int quantidade) // o construtor obria a instanciacao no momento da construcao
+        {
+            _nome = nome;
+            _preco = preco;
+            _quantidade = quantidade;
+        }
+        // ********************
+
+
+        // Encapsulamentos *********************
+
+        public double GetPreco()
+        {
+            return _preco;
+        }
+        public int GetQuantidade()
+        {
+            return _quantidade;
+        }
+        public string GetNome()
+        {
+            return _nome;
+        }
+        public void SetNome(string nome)
+        {
+            if (nome != null && nome.Length > 1) // com o atributo "set" posso atrinuir uma logica para aceitar ou regeitar alteracoes
+            {
+                _nome = nome;
+            }
+        }
+        // **************************************
+
+
+
+
+
+
+
+        public double ValorTotalEmEstoque()
+        {
+            return _preco * _quantidade;
+        }
+
+        public void AdicionarProdutos(int quantidade) // esta funcao e 'void' pq ela nao retorna valor, mas ela altera uma variavel
+        {
+            _quantidade = _quantidade + quantidade;
+        }
+
+        public void RemoverProdutos(int quantidade) // esta funcao e 'void' pq ela nao retorna valor, mas ela altera uma variavel
+        {
+            _quantidade = _quantidade - quantidade;
+        }
+
+        public override string ToString()
+        {
+            return _nome
+                + ", $ "
+                + _preco.ToString("F2", CultureInfo.InvariantCulture)
+                + ", "
+                + _quantidade
+                + " unidades, Total: $ "
+                + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+    }
+}
+
+
---
+++ 087-Exemplo Encapsulamento/087-Exemplo Encapsulamento/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+namespace Curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Produto p = new Produto("TV", 500.00, 10);
+
+            p.SetNome("TV 4K"); // via metodo "set" posso mudar o nome do atributo privado
+
+            Console.WriteLine(p.GetNome()); // para acessar o atributo, so pelo metodo "get"
+
+
+        }
+    }
+}
+
+
