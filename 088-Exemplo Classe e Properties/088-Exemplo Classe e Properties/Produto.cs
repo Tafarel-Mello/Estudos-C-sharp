7296fe08d2e543079cc7eefca3830490dca0c19c
Tafarel Mello <tafarel.luis.mello@gmail.com>
2022-08-18 20:22:47 -0300

    Commit

---
+++ 088-Exemplo Classe e Properties/088-Exemplo Classe e Properties/Produto.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Curso
+{
+    class Produto
+    {
+        private string _nome;
+        private double _preco;
+        private int _quantidade;
+
+
+
+
+        // construtores ***************
+        public Produto()
+        {
+
+        }
+
+        public Produto(string nome, double preco, int quantidade) // o construtor obria a instanciacao no momento da construcao
+        {
+            _nome = nome; // esse valor entra como value no properties abaixo
+            _preco = preco;
+            _quantidade = quantidade;
+        }
+        // ********************
+
+
+        // Encapsulamentos com properties *********************
+
+       
+        public string Nome
+        {
+            get { return _nome; }
+            set {
+                if(value != null && value.Length > 1) // value vai receber o valor de "nome" que foi instanciado
+                {
+                    _nome = value;
+                }
+            }
+        }
+
+        public double Preco
+        {
+            get { return _preco;  }
+        }
+        public int Quantidade
+        {
+            get { return _quantidade; }
+        }
+
+
+        // **************************************
+
+
+
+
+
+
+
+        public double ValorTotalEmEstoque()
+        {
+            return _preco * _quantidade;
+        }
+
+        public void AdicionarProdutos(int quantidade) // esta funcao e 'void' pq ela nao retorna valor, mas ela altera uma variavel
+        {
+            _quantidade = _quantidade + quantidade;
+        }
+
+        public void RemoverProdutos(int quantidade) // esta funcao e 'void' pq ela nao retorna valor, mas ela altera uma variavel
+        {
+            _quantidade = _quantidade - quantidade;
+        }
+
+        public override string ToString()
+        {
+            return _nome
+                + ", $ "
+                + _preco.ToString("F2", CultureInfo.InvariantCulture)
+                + ", "
+                + _quantidade
+                + " unidades, Total: $ "
+                + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+    }
+}
+
+
---
+++ 088-Exemplo Classe e Properties/088-Exemplo Classe e Properties/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+
+namespace Curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Produto p = new Produto("TV", 500.00, 10);
+
+            p.Nome = "TV 4K"; // via metodo "set" posso mudar o nome do atributo privado
+
+            Console.WriteLine(p.Nome); // para acessar o atributo, so pelo metodo "get"
+            Console.WriteLine(p.Preco);
+
+
+        }
+    }
+}
+
+
+
