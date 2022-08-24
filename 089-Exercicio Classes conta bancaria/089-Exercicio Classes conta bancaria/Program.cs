96a44acd54810164be7887c050ff3de48e2bc585
Tafarel Mello <tafarel.luis.mello@gmail.com>
2022-08-23 21:06:28 -0300

    Commit

---
+++ 089-Exercicio Classes conta bancaria/089-Exercicio Classes conta bancaria/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+
+namespace Curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numeroConta;
+            string nomeTitular;
+            char depositoInicial;
+            float saldoInicial, saldo;
+
+            Console.WriteLine("Entre o numero da conta: ");
+            numeroConta = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Entre o titular da conta: ");
+            nomeTitular = Console.ReadLine();
+
+            Console.WriteLine("Havera deposito inicial (s/n)? ");
+            depositoInicial = char.Parse(Console.ReadLine());
+
+            Console.WriteLine("Entre o valor de deposito inicial: ");
+            saldoInicial = float.Parse(Console.ReadLine());
+
+
+
+        }
+    }
+}
