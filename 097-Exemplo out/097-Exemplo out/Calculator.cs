122c4a862a059c0ed1891a1935bea72139c47270
Tafarel Mello <tafarel.luis.mello@gmail.com>
2022-09-10 20:21:34 -0300

    Commit

---
+++ 097-Exemplo out/097-Exemplo out/Calculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Curso
+{
+    class Calculator
+    {
+        public static void Triplo(int origin, out int result)
+        {
+            result = origin * 3;
+        }
+    }
+}
---
+++ 097-Exemplo out/097-Exemplo out/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace Curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10; // parametro de entrada - deve ser iniciada
+            int triplo; // local onde o resultado sera instanciado - nao precisa ser iniciada
+
+            Calculator.Triplo(a, out triplo);
+            Console.WriteLine(triplo);
+        }
+    }
+    /*
+     * Com a funcao "out" temos quase o mesmo comportamento que no "ref"
+     * com a diferenca que o resultado do metodo chamado em "Calculator.Triplo" sera salvo
+     * em outra variavel, que nao precisa ser iniciada previamente, e nao na variavel original "a".
+     */
+}
