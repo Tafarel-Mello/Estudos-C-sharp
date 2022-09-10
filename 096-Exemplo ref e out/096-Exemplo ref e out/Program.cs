26f1197855de8d39b138b95180dc600f8ea34de9
Tafarel Mello <tafarel.luis.mello@gmail.com>
2022-09-10 20:09:45 -0300

    Commit

---
+++ 096-Exemplo ref e out/096-Exemplo ref e out/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+
+namespace Curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+
+            Calculator.Triple(ref a);
+            Console.WriteLine(a);
+        }
+    }
+
+    /*
+     * Sem a utilizacao do comando "ref" a classe "Calculator" faria o calculo mas nao faria
+     * a alteracao desejada na variavel no escopo principal do programa. O comando "ref" torna
+     * a funcao "Calculator.Triple" uma referencia para a variavel passada como parametro, fazendo
+     * assim a acao desejada.
+     */
+}
