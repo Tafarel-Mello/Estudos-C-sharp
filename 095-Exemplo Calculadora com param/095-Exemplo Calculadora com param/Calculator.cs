4be618d01eb11a952990c35942dc25d7ba85fb33
Tafarel Mello <tafarel.luis.mello@gmail.com>
2022-09-10 20:01:00 -0300

    Commit

---
+++ 095-Exemplo Calculadora com param/095-Exemplo Calculadora com param/Calculator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Curso
+{
+    class Calculator
+    {
+        public static int Soma(params int[] numeros)
+        {
+            int soma = 0;
+            for(int i = 0; i < numeros.Length; i++)
+            {
+                soma = soma + numeros[i];
+            }
+            return soma;
+        }
+    }
+}
+
+/*
+ A instrucao "params" ja diz para funcao que os parametros podem variar
+e o comando "for" vai criar o vetor em funcao dos parametros entrados para a funcao
+desta forma nao se faz necessario instanciar um vetor com o "new" no programa principal
+para chamar a funcao desejada
+ */
---
+++ 095-Exemplo Calculadora com param/095-Exemplo Calculadora com param/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+namespace Curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int s1 = Calculator.Soma(2, 3);
+            int s2 = Calculator.Soma(6, 9);
+            int s3 = Calculator.Soma(5, 5);
+
+            Console.WriteLine(s1);
+            Console.WriteLine(s2);
+            Console.WriteLine(s3);
+        }
+    }
+}
+/*
+  Se nao usassemos o "params" aqui, para a entrada de valores de parametros para a chamada de funcao
+Calculator.Soma, deveriamos instanciar o vetor de parametros e tmbm na classe, prever todas as possibilidades 
+de variacoes de entrada de parametros. Ou seja, para 2 parametros uma funcao, para 3 outra e assim por diante.
+ */
