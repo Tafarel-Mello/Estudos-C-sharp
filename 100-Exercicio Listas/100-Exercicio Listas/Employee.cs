a6ef2c590e3d6fcac2394bdc6d39ec8631c0c718
Tafarel Mello <tafarel.luis.mello@gmail.com>
2022-09-11 17:40:19 -0300

    Commit

---
+++ 100-Exercicio Listas/100-Exercicio Listas/Employee.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Curso
+{
+    class Employee
+    {
+        public int Id { get; private set; }
+        public string Name { get; set; }
+        public double Salary { get; private set; }
+
+        public Employee(int id, string name, double salary)
+        {
+            Id = id;
+            Name = name;
+            Salary = salary;
+        }
+
+        public override string ToString()
+        {
+            return Id +
+                ", " + 
+                Name +
+                ", " +
+                Salary.ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+    }
+
+}
---
+++ 100-Exercicio Listas/100-Exercicio Listas/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+
+namespace Curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Employee> list = new List<Employee>(); // instaciamento da lista
+
+            Console.Write("How many employees will be registered? ");
+            int n = int.Parse(Console.ReadLine());
+
+            for(int i = 1; i <= n; i++)
+            {
+                Console.WriteLine("Employee #" + i + ":");
+                Console.Write("Id: ");
+                int id = int.Parse(Console.ReadLine());
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Salary: ");
+                double salary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                list.Add(new Employee(id, name, salary)); // instanciamento do elemento "Employee"
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("Updated list of employees:");
+            foreach(Employee emp in list)
+            {
+                Console.WriteLine(emp);
+            }
+
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
