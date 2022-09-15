c478516151de7f45cba484cd425e96237e144bc1
Tafarel Mello <tafarel.luis.mello@gmail.com>
2022-09-14 21:33:28 -0300

    Commit

---
+++ 104-Exemplo DateTime/104-Exemplo DateTime/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime d1 = DateTime.Now;
+            DateTime d2 = new DateTime(2018, 11, 25);
+            DateTime d3 = new DateTime(2018, 11, 25, 20, 45, 3);
+            DateTime d4 = DateTime.Today;
+
+
+            Console.WriteLine(d1);
+            Console.WriteLine(d2);
+            Console.WriteLine(d3);
+
+            DateTime d5 = DateTime.Parse("15/08/2022");
+            Console.WriteLine(d5);
+        }
+    }
+}
