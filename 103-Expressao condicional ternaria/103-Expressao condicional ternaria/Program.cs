fa57b0c5b6bc9706875b28db48408a634b769cd1
Tafarel Mello <tafarel.luis.mello@gmail.com>
2022-09-14 21:04:14 -0300

    Commit

---
+++ 103-Expressao condicional ternaria/103-Expressao condicional ternaria/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Globalization;
+
+namespace Curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * sintaxe => (condicao) ? valor se verdadeiro : valor se for falso
+             * alternativa para if/else
+             */
+
+            double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double precoFinal;
+
+            //double desconto;
+            /*
+            if(preco < 20.0)
+            {
+                desconto = preco * 0.1;
+            }
+            else
+            {
+                desconto = preco * 0.05;
+            }
+            */
+
+            double desconto = (preco < 20.0) ? preco * 0.1 : preco * 0.05;
+
+
+
+
+
+            precoFinal = preco - desconto;
+            Console.WriteLine(preco);
+        }
+    }
+}
