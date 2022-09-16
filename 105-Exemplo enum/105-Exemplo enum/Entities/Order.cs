d9be5f352578c488ec768efac0feed5e45547b97
Tafarel Mello <tafarel.luis.mello@gmail.com>
2022-09-16 15:55:39 -0300

    Commit

---
+++ 105-Exemplo enum/105-Exemplo enum/Entities/Order.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using _105_Exemplo_enum.Entities.Enum;
+
+namespace _105_Exemplo_enum.Entities
+{
+    internal class Order
+    {
+        public int Id { get; set; }
+        public DateTime Moment { get; set; }
+        public OrderStatus Status { get; set; }
+
+        public override string ToString()
+        {
+            return Id + ", " + Moment + ", " + Status;
+        }
+    }
+}
---
+++ 105-Exemplo enum/105-Exemplo enum/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using _105_Exemplo_enum.Entities;
+using _105_Exemplo_enum.Entities.Enum;
+
+namespace Curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Order order = new Order
+            {
+                Id = 1080,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PendingPayment
+            };
+
+            Console.WriteLine(order);
+
+            string txt = OrderStatus.PendingPayment.ToString();
+
+            Console.WriteLine(txt);
+
+            OrderStatus os = Enum.Parse<OrderStatus>("Delivered");
+
+            Console.WriteLine(os);
+        }
+    }
+}
